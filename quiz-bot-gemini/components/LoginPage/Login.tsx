bda6532b3ebb895e6cf1dffc6045b23555ac9d13
rukaiah-edhah <rukaiah.edhah@yahoo.com>
2024-04-16 14:35:26 -0400

    Updated sign-in & login to include additional authentication providers

--- quiz-bot-gemini/components/LoginPage/Login.tsx
+++ quiz-bot-gemini/components/LoginPage/Login.tsx
@@ -1,74 +1,115 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Link from "next/link";
 import { Button } from "../ui/button";
-import axios from 'axios'; // Import axios for making HTTP requests
+import axios from "axios"; // Import axios for making HTTP requests
 import { FaGoogle } from "react-icons/fa";
+import { FaApple } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 export default function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(''); // State to handle error messages
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(""); // State to handle error messages
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        try {
-            // Make a POST request to your API endpoint
-            const response = await axios.post('/api/login', { email, password });
-            console.log(response.data); // Process the response as needed
-            // Redirect or update UI upon successful login
-        } catch (error) {
-            if (axios.isAxiosError(error) && error.response) {
-                // Handle response errors (e.g., 401 Unauthorized)
-                setError(error.response.data.message);
-            } else {
-                // Handle other errors (network issues, etc.)
-                setError('An unexpected error occurred');
-            }
-        }
-    };
-    return (
-            <div className="md:max-w-2xl max-w-md p-20 border rounded-md bg-white shadow-lg"> {/* if we want a darker color then we should use bg-gray-900 */}
-                <form onSubmit={handleSubmit} className="space-y-6">
-                    <h1 className="text-5xl font-bold text-center mb-7 text-purple-500">Welcome Back!</h1>
-                    <p className="text-md md:text-lg text-center mb-8 text-black">Provide your credentials to kickstart the quiz</p>
-                    <input 
-                        type="email" 
-                        id="email" 
-                        name="email"
-                        placeholder="Enter your email address"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="w-full border-2 border-purple-300 rounded-md py-3 px-7 text-black focus:outline-none focus:border-purple-500"
-                        required 
-                    />
-                    <input 
-                        type="password"
-                        id="password"
-                        name="password"
-                        placeholder="Enter your password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="w-full border-2 border-purple-300 rounded-md py-3 px-7 text-black focus:outline-none focus:border-purple-500"
-                        required 
-                    />
-                    <div className="text-right">
-                        <Link href="" className="hover:text-purple-500 text-black text-sm">Forgot your password?</Link>
-                    </div>
-                    <Button type="submit" className="w-full bg-purple-500 py-6 text-white rounded-md hover:bg-purple-600">
-                        Log in
-                    </Button>
-                    <div className="w-full flex items-center justify-center relative py-2">
-                        <div className="w-full h-[1px] bg-black"></div>
-                        <p className="absolute text-black/80 bg-[white] p-4">or</p>
-                    </div>
-                    <Link href="" className="flex items-center justify-center bg-purple-500 hover:bg-purple-600 py-4 rounded-md ">
-                        <FaGoogle className="mr-2"/><span>Log in with Google</span>
-                    </Link>
-                </form>
-                <div className="text-center mt-4 text-black">
-                    <p>New here? <Link href="/signup" className="text-purple-500 hover:text-purple-600">sign up</Link></p>
-                </div>
-            </div>
-    );
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      // Make a POST request to your API endpoint
+      const response = await axios.post("/api/login", { email, password });
+      console.log(response.data); // Process the response as needed
+      // Redirect or update UI upon successful login
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        // Handle response errors (e.g., 401 Unauthorized)
+        setError(error.response.data.message);
+      } else {
+        // Handle other errors (network issues, etc.)
+        setError("An unexpected error occurred");
+      }
+    }
+  };
+  return (
+    <div className="md:max-w-2xl max-w-md p-20 border rounded-md bg-white shadow-lg">
+      {" "}
+      {/* if we want a darker color then we should use bg-gray-900 */}
+      <form onSubmit={handleSubmit} className="space-y-6">
+        <h1 className="text-5xl font-bold text-center mb-7 text-purple-500">
+          Welcome Back!
+        </h1>
+        <p className="text-md md:text-lg text-center mb-8 text-black">
+          Provide your credentials to kickstart the quiz
+        </p>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter your email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full border-2 border-purple-300 rounded-md py-3 px-7 text-black focus:outline-none focus:border-purple-500"
+          required
+        />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full border-2 border-purple-300 rounded-md py-3 px-7 text-black focus:outline-none focus:border-purple-500"
+          required
+        />
+        <div className="text-right">
+          <Link href="" className="hover:text-purple-500 text-black text-sm">
+            Forgot your password?
+          </Link>
+        </div>
+        <Button
+          type="submit"
+          className="w-full bg-purple-500 hover:bg-purple-600 py-6 text-white rounded-md"
+        >
+          Log in
+        </Button>
+        <div className="w-full flex items-center justify-center relative py-2">
+          <div className="w-full h-[1px] bg-black"></div>
+          <p className="absolute text-black/80 bg-[white] p-4">or</p>
+        </div>
+        <div className="flex justify-center space-x-4 py-4">
+          <Link
+            href=""
+            className="flex items-center justify-center bg-red-500 hover:bg-red-600 py-3 px-2 md:px-8 rounded-md"
+          >
+            <FaGoogle className="mr-2" />
+            <span>Google</span>
+          </Link>
+          <Link
+            href=""
+            className="flex items-center justify-center bg-blue-600 hover:bg-blue-700 py-3 px-2 md:px-8 rounded-md"
+          >
+            <FaGithub className="mr-2" />
+            <span>GitHub</span>
+          </Link>
+          <Link
+            href=""
+            className="flex items-center justify-center bg-blue-400 hover:bg-blue-500 py-3 px-2 md:px-8 rounded-md"
+          >
+            <FaApple className="mr-2" />
+            <span>Apple</span>
+          </Link>
+        </div>
+      </form>
+      <div className="text-center mt-4 text-black">
+        <p>
+          New here?{" "}
+          <Link
+            href="/signup"
+            className="text-purple-500 hover:text-purple-600"
+          >
+            sign up
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
--- quiz-bot-gemini/components/SignupPage/Signup.tsx
+++ quiz-bot-gemini/components/SignupPage/Signup.tsx
@@ -1,104 +1,141 @@
-'use client';
-import React, { useState } from 'react';
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import { Button } from "../ui/button"; // Assuming Button is a custom component
-import { FaGoogle } from 'react-icons/fa';
+import { FaApple, FaGithub, FaGoogle } from "react-icons/fa";
 
 export default function Signup() {
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-        confirmPassword: '',
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [message, setMessage] = useState("");
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMessage("");
+    setIsSuccess(false);
+
+    const response = await fetch("/api/signup", {
+      // Adjust the URL as needed
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
     });
-    const [message, setMessage] = useState('');
-    const [isSuccess, setIsSuccess] = useState(false);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value,
-        }));
-    };
+    const data = await response.json();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setMessage('');
-        setIsSuccess(false);
+    if (response.ok) {
+      setMessage("User created successfully. Please login.");
+      setIsSuccess(true);
+      setFormData({ email: "", password: "", confirmPassword: "" });
+      // Optionally, redirect the user to the login page or another page
+      // window.location.href = '/login';
+    } else {
+      setMessage(data.message || "An error occurred, please try again.");
+    }
+  };
 
-        const response = await fetch('/api/signup', { // Adjust the URL as needed
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(formData),
-        });
-
-        const data = await response.json();
-        
-        if (response.ok) {
-            setMessage('User created successfully. Please login.');
-            setIsSuccess(true);
-            setFormData({ email: '', password: '', confirmPassword: '' });
-            // Optionally, redirect the user to the login page or another page
-            // window.location.href = '/login';
-        } else {
-            setMessage(data.message || 'An error occurred, please try again.');
-        }
-    };
-
-    return (
-        <div className="md:max-w-2xl max-w-md p-20 border rounded-md bg-white shadow-lg">
-            <form onSubmit={handleSubmit} className="space-y-6">
-                <h1 className="text-5xl font-bold text-center mb-7 text-purple-500">Create Your Account</h1>
-                <p className="text-md md:text-lg text-center mb-8 text-black">Sign up to start the adventure</p>
-                <input 
-                    type="email" 
-                    id="email" 
-                    name="email"
-                    placeholder="Enter your email address"
-                    value={formData.email}
-                    onChange={handleChange}
-                    className="w-full border-2 border-purple-300 rounded-md py-3 px-7 text-black focus:outline-none focus:border-purple-500"
-                    required 
-                />
-                <input 
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="Enter your password"
-                    value={formData.password}
-                    onChange={handleChange}
-                    className="w-full border-2 border-purple-300 rounded-md py-3 px-7 text-black focus:outline-none focus:border-purple-500"
-                    required 
-                />
-                <input 
-                    type="password"
-                    id="confirmPassword"
-                    name="confirmPassword"
-                    placeholder="Confirm your password"
-                    value={formData.confirmPassword}
-                    onChange={handleChange}
-                    className="w-full border-2 border-purple-300 rounded-md py-3 px-7 text-black focus:outline-none focus:border-purple-500"
-                    required 
-                />
-                {message && (
-                    <div className={`text-center mt-2 text-${isSuccess ? 'green-600' : 'red-600'}`}>
-                        <p>{message}</p>
-                    </div>
-                )}
-                <Button type="submit" className="w-full bg-purple-500 py-6 text-white rounded-md hover:bg-purple-600">
-                    Sign Up
-                </Button>
-                <div className="w-full flex items-center justify-center relative py-2">
-                    <div className="w-full h-[1px] bg-black"></div>
-                    <p className="absolute text-black/80 bg-[white] p-4">or</p>
-                </div>
-                <Link href="" className="flex items-center justify-center bg-purple-500 hover:bg-purple-600 py-4 rounded-md">
-                    <FaGoogle className="mr-2"/><span>Sign up with Google</span>
-                </Link>
-            </form>
-            <div className="text-center mt-4 text-black">
-                <p>Already have an account? <Link href="/login" className="text-purple-500 hover:text-purple-600">Log in</Link></p>
-            </div>
+  return (
+    <div className="md:max-w-2xl max-w-md p-20 border rounded-md bg-white shadow-lg">
+      <form onSubmit={handleSubmit} className="space-y-6">
+        <h1 className="text-5xl font-bold text-center mb-7 text-purple-500">
+          Create Your Account
+        </h1>
+        <p className="text-md md:text-lg text-center mb-8 text-black">
+          Sign up to start the adventure
+        </p>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter your email address"
+          value={formData.email}
+          onChange={handleChange}
+          className="w-full border-2 border-purple-300 rounded-md py-3 px-7 text-black focus:outline-none focus:border-purple-500"
+          required
+        />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter your password"
+          value={formData.password}
+          onChange={handleChange}
+          className="w-full border-2 border-purple-300 rounded-md py-3 px-7 text-black focus:outline-none focus:border-purple-500"
+          required
+        />
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          placeholder="Confirm your password"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          className="w-full border-2 border-purple-300 rounded-md py-3 px-7 text-black focus:outline-none focus:border-purple-500"
+          required
+        />
+        {message && (
+          <div
+            className={`text-center mt-2 text-${
+              isSuccess ? "green-600" : "red-600"
+            }`}
+          >
+            <p>{message}</p>
+          </div>
+        )}
+        <Button
+          type="submit"
+          className="w-full bg-purple-500 py-6 text-white rounded-md hover:bg-purple-600"
+        >
+          Sign Up
+        </Button>
+        <div className="w-full flex items-center justify-center relative py-2">
+          <div className="w-full h-[1px] bg-black"></div>
+          <p className="absolute text-black/80 bg-[white] p-4">or</p>
         </div>
-    );
+        <div className="flex justify-center space-x-4 py-4">
+          <Link
+            href=""
+            className="flex items-center justify-center bg-red-500 hover:bg-red-600 py-3 px-2 md:px-10 rounded-md"
+          >
+            <FaGoogle className="mr-2" />
+            <span>Google</span>
+          </Link>
+          <Link
+            href=""
+            className="flex items-center justify-center bg-blue-600 hover:bg-blue-700 py-3 px-2 md:px-10 rounded-md"
+          >
+            <FaGithub className="mr-2" />
+            <span>GitHub</span>
+          </Link>
+          <Link
+            href=""
+            className="flex items-center justify-center bg-blue-400 hover:bg-blue-500 py-3 px-2 md:px-10 rounded-md"
+          >
+            <FaApple className="mr-2" />
+            <span>Apple</span>
+          </Link>
+        </div>
+      </form>
+      <div className="text-center mt-4 text-black">
+        <p>
+          Already have an account?{" "}
+          <Link href="/login" className="text-purple-500 hover:text-purple-600">
+            Log in
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
