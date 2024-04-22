050017d56edc9fe8d7b59401e3ea03d66ca32b3e
rukaiah-edhah <rukaiah.edhah@yahoo.com>
2024-04-22 11:49:46 -0400

    Validated and sanitized user inputs :)

--- quiz-bot-gemini/app/api/chat/route.ts
+++ quiz-bot-gemini/app/api/chat/route.ts
@@ -1,27 +1,48 @@
-import { GoogleGenerativeAI } from '@google/generative-ai';
-import { GoogleGenerativeAIStream, Message, StreamingTextResponse } from 'ai';
- 
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || '');
- 
-export const runtime = 'edge';
- 
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAIStream, Message, StreamingTextResponse } from "ai";
+import { escape } from "validator";
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || "");
+
+export const runtime = "edge";
+
 const buildGoogleGenAIPrompt = (messages: Message[]) => ({
   contents: messages
-    .filter(message => message.role === 'user' || message.role === 'assistant')
-    .map(message => ({
-      role: message.role === 'user' ? 'user' : 'model',
-      parts: [{ text: message.content }],
-    })),
+    .filter((message) => ["user", "assistant"].includes(message.role))
+    .map((message) => {
+      // Validating message content by 1) ensureing each message content is a string
+      // 2) preventing long inputs that could be used in DoS attacks
+      if (
+        typeof message.content !== "string" ||
+        message.content.length > 1000
+      ) {
+        console.error(`Invalid message content: ${message.content}`);
+        throw new Error("Invalid message content");
+      }
+      const sanitizedContent: string = escape(message.content); // To prevent XSS attacks ;)
+      return {
+        role: message.role === "user" ? "user" : "model",
+        parts: [{ text: sanitizedContent }],
+      };
+    }),
 });
- 
+
 export async function POST(req: Request) {
-  const { messages } = await req.json();
- 
-  const geminiStream = await genAI
-    .getGenerativeModel({ model: 'gemini-pro' })
-    .generateContentStream(buildGoogleGenAIPrompt(messages));
- 
-  const stream = GoogleGenerativeAIStream(geminiStream);
- 
-  return new StreamingTextResponse(stream);
+  try {
+    const { messages } = await req.json();
+    const geminiStream = await genAI
+      .getGenerativeModel({ model: "gemini-pro" })
+      .generateContentStream(buildGoogleGenAIPrompt(messages));
+
+    const stream = GoogleGenerativeAIStream(geminiStream);
+    return new StreamingTextResponse(stream);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(`Error processing request: ${error.message}`);
+      return new Response(error.message, { status: 400 });
+    } else {
+      console.error(`An unexpected error occurred: ${error}`);
+      return new Response("An unexpected error occurred", { status: 500 });
+    }
+  }
 }
